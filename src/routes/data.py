026c95911f3e45a3d22006f66363664a56665893
qandos-git <qandos@Noor.>
2025-04-08 11:41:31 +0300

    File Processing

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
+from .schemes.data import ProcessRequest
 import os
 import aiofiles
 from models import ResponseSignal
@@ -55,4 +56,29 @@ async def upload_data(project_id:str,
         }
     )
 
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, prosess_request:ProcessRequest):
+    file_id = prosess_request.file_id
+    chunk_size = prosess_request.chunk_size
+    overlap_size = prosess_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_id=file_id,
+        file_content=file_content,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                            content={
+                                 "signal" : ResponseSignal.PROCESSING_FAILED.value
+                            })
+
+    return file_chunks
 
